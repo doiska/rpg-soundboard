976954a3f0dd5ef0a2f38db8d72821ac05a12457
doiská <doiskka@gmail.com>
2023-09-10 18:44:23 -0300

    refactor: moved from express to fastify

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,60 +1,59 @@
-import express from "express";
-import {Server} from 'socket.io';
+//TODO: https://socket.io/pt-br/docs/v4/typescript/
+
 import {setupFileRouter} from "express-router-fs";
-import http from 'http';
-import {env} from "./lib/env";
 import * as fs from "fs";
 import * as path from "path";
+import { fastify } from "fastify";
+import { default as fastifyIo } from "fastify-socket.io";
+import fastifyStatic from "@fastify/static";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import {env} from "./lib/env.js";
 
-const app = express();
+export var __filename = fileURLToPath(import.meta.url);
+export var __dirname = dirname(fileURLToPath(import.meta.url));
 
-const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: '*',
-    }
+const app = fastify();
+
+app.register(fastifyIo as never);
+app.register(fastifyStatic, {
+    root: path.join(__dirname, '..', 'public')
 });
 
-setupFileRouter(app as never, {
+await setupFileRouter(app as never, {
     directory: __dirname + "/routes",
+});
+
+app.listen({
+    port: Number(env.PORT)
 }).then(() => {
-    console.log("[File Router] All routes loaded!");
-});
+    app.io.on('connection', (socket) => {
+        socket.on('connect-room', (room: string) => {
+            console.log(`Connected to room ${room}`);
+            socket.join(room);
+        });
 
-io.on('connection', (socket) => {
-    socket.on('connect-room', (room: string) => {
-        console.log(`Connected to room ${room}`);
-        socket.join(room);
+        socket.on('disconnect-room', (room: string) => {
+            console.log(`Disconnected from room ${room}`);
+            socket.leave(room);
+        });
+
+        socket.on('disconnect', () => {
+            console.log('Disconnected!');
+        });
+
+        //TODO: use fetch instead of socket to list the songs
+        socket.on('list-songs', (room: string) => {
+            const files = fs.readdirSync('./public/sounds', {
+                recursive: true,
+                withFileTypes: true
+            }).filter(file => file.isFile() && file.name.split('.').pop() === 'mp3').map(file => file.name);
+
+            socket.emit('list-songs-response', files);
+        });
+
+        socket.on('song-control-playback', (room: string, action: string, data: any) => {
+            app.io.to(room).emit('song-control-set-playback', action, data);
+        })
     });
-
-    socket.on('disconnect-room', (room: string) => {
-        console.log(`Disconnected from room ${room}`);
-        socket.leave(room);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('Disconnected!');
-    });
-
-    //TODO: use fetch instead of socket to list the songs
-    socket.on('list-songs', (room: string) => {
-        const files = fs.readdirSync('./public/sounds', {
-            recursive: true,
-            withFileTypes: true
-        }).filter(file => file.isFile() && file.name.split('.').pop() === 'mp3').map(file => file.name);
-
-        socket.emit('list-songs-response', files);
-    });
-
-    socket.on('song-control-playback', (room: string, action: string, data: any) => {
-        io.to(room).emit('song-control-set-playback', action, data);
-    })
-});
-
-server.listen(env.PORT, () => {
-    console.log(`Listening on port ${env.PORT}`);
-});
-
-console.log(path.join(__dirname, '..', 'public'))
-
-app.use(express.static('public'));
+})
---
+++ packages/server/src/routes/user/[userId]/sounds.ts
@@ -0,0 +1,5 @@
+import {FastifyReply, FastifyRequest} from "fastify";
+
+export function POST(request: FastifyRequest, response: FastifyReply) {
+
+}
